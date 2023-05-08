d6abec95655f87d61a55f9867087566eab68a6a7
dbN2 <81189816+dbN2@users.noreply.github.com>
2023-05-08 16:20:04 -0700

    Update cors

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const statesRouter = require('./routes/states');
@@ -5,8 +6,7 @@ const rootRouter = require('./routes/root.js');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-
-require('dotenv').config();
+const corsOptions = require('./config/corsOptions');
 
 const app = express();
 
@@ -16,12 +16,12 @@ mongoose.set('strictQuery', false);
 // connect to MongoDB
 const dbconn = process.env.DATABASE_URI;
 mongoose.connect(dbconn
-, { useNewUrlParser: true })
+)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
 // define routes
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use('/', rootRouter);
@@ -30,22 +30,20 @@ app.use('/states', statesRouter);
 app.use((req, res, next) => {
     res.status(404);
   
-    // respond with HTML if the client accepts text/html
-
-    // respond with JSON if the client accepts application/json
     if (req.accepts('json')) {
       res.json({ error: '404 Not Found' });
       return;
     }
-    if (req.accepts('html')) {
+
+    else if (req.accepts('html')) {
       const filePath = path.join(__dirname, '404.html');
       res.sendFile(filePath);
       return;
     }
   
-  
-    // default to plain text
-    res.type('txt').send('404 Not Found');
+    else{
+      res.type('txt').send('404 Not Found');
+    }
   });
 
 // start the server
